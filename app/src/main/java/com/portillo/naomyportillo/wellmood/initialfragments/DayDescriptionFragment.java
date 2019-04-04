0d851cd5afe3de7f12d029b0f92dfd39dfac30d0
rosannaNaomy <naomypursuit.org>
2019-04-03 20:09:09 -0400

    created fragments

---
+++ app/src/main/java/com/portillo/naomyportillo/wellmood/initialfragments/DayDescriptionFragment.java
@@ -0,0 +1,53 @@
+package com.portillo.naomyportillo.wellmood.initialfragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.portillo.naomyportillo.wellmood.R;
+
+
+public class DayDescriptionFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public DayDescriptionFragment() {
+        // Required empty public constructor
+    }
+
+    public static DayDescriptionFragment newInstance(String param1, String param2) {
+        DayDescriptionFragment fragment = new DayDescriptionFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_day_description, container, false);
+    }
+
+}
